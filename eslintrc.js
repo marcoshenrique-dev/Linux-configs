69251a071834efd7c5fe5b8f955d7477b9cbfd4b
Marcos Henrique <51785898+marcoshenrique-dev@users.noreply.github.com>
2020-08-11 18:20:56 -0300

    Configuration for ESLINT 😎

---
+++ eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+  },
+};
